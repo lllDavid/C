3f7b8d499b4cc2b6f91606d7fa6feca4dae93b93
lllDavid <imperfectrising5527@gmail.com>
2025-04-23 01:03:47 +0200

    sync

---
+++ pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;         
+    int *ptr = &number;      
+
+    printf("Value of number: %d\n", number);
+    printf("Address of number: %p\n", &number);
+    printf("Value of ptr (address): %p\n", ptr);
+    printf("Value pointed to by ptr: %d\n", *ptr);  
+
+    *ptr = 20;
+    printf("New value of number (after pointer update): %d\n", number);
+
+    return 0;
+}
